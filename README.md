06442b8186887e7a0136717a71151ecff1a0ed57
Julian Kaffke <julian@melopos.com>
2015-09-17 15:04:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Usage
+===========
+Right now this is early alpha! Do **not** use in production if you're not sure, what yo do!
---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,22 @@
+# Configure
+Dragonfly.app(:engine).configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret '7cf130ac6e44c1c86bb7c092ce1c1a4722801a2a0259f5683100cd59be318dc6'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware, :engine
